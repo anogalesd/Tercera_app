3d77c0a451362e733e6decef834449726cf0e0c1
anogalesd <anogales@birt.eus>
2025-10-01 19:10:36 +0200

    Subiendo tabs

---
+++ src/app/tabs/tab4/tab4.module.ts
@@ -0,0 +1,21 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab4Page } from './tab4.page';
+import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
+import { TabsPageModule } from '../tabs.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ExploreContainerComponentModule,
+    RouterModule.forChild([{ path: '', component: Tab4Page }]),
+    TabsPageModule,
+  ],
+  declarations: [Tab4Page]
+})
+export class Tab4PageModule {}
